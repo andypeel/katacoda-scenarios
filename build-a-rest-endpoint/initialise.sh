65f4e2e2b4f208377438c1371a92314918068d32
Andy Peel <anpeel@theworkshop.com>
2022-10-26 18:39:50 +0100

    cd

--- build-a-rest-endpoint/initialise.sh
+++ build-a-rest-endpoint/initialise.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 echo "Installing OpenJDK 11 and Maven"
-apt --yes install maven
-cd assets
-clear
-echo "Welcome to the tutorial, all the dependencies are installed, and you should be good to go!"
+apt --yes install maven && cd assets && clear && echo "Welcome to the tutorial, all the dependencies are installed, and you should be good to go!"
 
 
 
