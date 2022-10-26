5d1afa19b4335c215b941fc65fbb1de646835b3b
Andy Peel <anpeel@theworkshop.com>
2022-10-26 15:28:17 +0100

    In migrating to Killercoda, we have to manage the full environment above the OS

--- build-a-rest-endpoint/initialise.sh
+++ build-a-rest-endpoint/initialise.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "Installing OpenJDK 11"
-apt --yes install openjdk-11-jre-headless
-echo "Installing Maven"
-apt --yes install maven
-clear && echo "Welcome to the tutorial, all the dependencies are installed, and you should be good to go!"
+#apt --yes install openjdk-11-jre-headless
+#echo "Installing Maven"
+#apt --yes install maven
+#clear && echo "Welcome to the tutorial, all the dependencies are installed, and you should be good to go!"
 
 
 
