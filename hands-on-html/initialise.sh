c107ea30f27de9ff2ffe7dd8a3fb6e42b5056109
Andy Peel <anpeel@theworkshop.com>
2023-08-08 15:27:44 +0100

    Attempt to clean bash script that probably won't work

--- hands-on-html/initialise.sh
+++ hands-on-html/initialise.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Installing Apache HTTPD"
-apt --yes install apache2
-rm -rf /var/www/html/
-ln -s /root/assets/html /var/www/html
-chmod a+x /root
-sed -i 's/Listen 80/Listen 8080/g' /etc/apache2/ports.conf
-systemctl restart apache2
-clear
-echo "Welcome to the tutorial, all the dependencies are installed, and you should be good to go!"
+apt --yes install apache2 \
+  && rm -rf /var/www/html/ \
+  && ln -s /root/assets/html /var/www/html \
+  && chmod a+x /root \
+  && sed -i 's/Listen 80/Listen 8080/g' /etc/apache2/ports.conf \
+  && systemctl restart apache2 \
+  && clear \
+  && echo "Welcome to the tutorial, all the dependencies are installed, and you should be good to go!"
