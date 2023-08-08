c0a2854375d011e527291558405b546489f1bfe0
Andy Peel <anpeel@theworkshop.com>
2023-08-08 15:07:01 +0100

    Open /root/assets/html to traversal by non-root users

--- hands-on-html/initialise.sh
+++ hands-on-html/initialise.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 echo "Installing Apache HTTPD"
-apt --yes install apache2 && rm -rf /var/www/html/ && ln -s /root/assets/html /var/www/html && clear && echo "Welcome to the tutorial, all the dependencies are installed, and you should be good to go!"
-
+apt --yes install apache2 && rm -rf /var/www/html/ && ln -s /root/assets/html /var/www/html && chmod a+x /root && clear && echo "Welcome to the tutorial, all the dependencies are installed, and you should be good to go!"
