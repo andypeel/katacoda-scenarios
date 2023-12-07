55bbd1d3220f54b85571cf808c8730e27de1ebfd
Andy Peel <anpeel@theworkshop.com>
2023-12-07 11:25:46 +0000

    Empty React scenario

---
+++ build-an-HTML5-UI/initialise.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Installing Apache HTTPD"
+
+apt --yes install npm \
+	&& npm install -g n
+	&& n latest
+	&& cd assets/cats-4-life
+	&& npm i
+	&& npm start
+  	&& echo "Welcome to the tutorial, all the dependencies are installed, and you should be good to go!"
---
+++ build-an-HTML5-UI/intro.md
@@ -0,0 +1,10 @@
+Welcome!
+
+You're about to make and change some HTML!
+
+The code you will be working with is running in the Cloud ☁️
+
+
+Start by opening the following webpage:
+
+{{TRAFFIC_HOST1_8080}}/
