d40647477043a2aaa50f6a6b501a9f232695cfef
Andy Peel <anpeel@theworkshop.com>
2023-12-07 11:36:14 +0000

    Fix init lineendings

--- build-an-HTML5-UI/initialise.sh
+++ build-an-HTML5-UI/initialise.sh
@@ -3,9 +3,9 @@
 echo "Installing Apache HTTPD"
 
 apt --yes install npm \
-	&& npm install -g n
-	&& n latest
-	&& cd assets/cats-4-life
-	&& npm i
-	&& npm start
+	&& npm install -g n \
+	&& n latest \
+	&& cd assets/cats-4-life \
+	&& npm i \
+	&& npm start \
   	&& echo "Welcome to the tutorial, all the dependencies are installed, and you should be good to go!"
