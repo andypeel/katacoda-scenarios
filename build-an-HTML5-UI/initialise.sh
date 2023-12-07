5218ab410a9053a91e7ad2eecec17ff7027d470c
Andy Peel <anpeel@theworkshop.com>
2023-12-07 12:34:58 +0000

    init fix

--- build-an-HTML5-UI/initialise.sh
+++ build-an-HTML5-UI/initialise.sh
@@ -6,7 +6,7 @@ echo "Setting up..."
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash \
 	&& export NVM_DIR="$HOME/.nvm" \
 	&& [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm \
-	&& [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion \
+	&& [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" \
 	&& nvm install v20 \
 	&& cd assets/cats-4-life \
 	&& npm i \
